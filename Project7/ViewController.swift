9f7873498c49e4df48dbc91d0f089f543da437c0
Subhrajyoti <subha.ionic3@gmail.com>
2020-07-04 01:25:10 +0530

    async modification

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -11,66 +11,122 @@ import UIKit
 class ViewController: UITableViewController {
     var petitions = [Petition]()
     var filteredPetitions = [Petition]()
-
+    var urlString: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(creditsTapped))
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterTapped))
-
-        let urlString: String
-
+        
+        
         if navigationController?.tabBarItem.tag == 0 {
             urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
         } else {
             urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
         }
-
+        performSelector(inBackground: #selector(fetchJSON), with: nil)
+    }
+    
+    @objc func fetchJSON() {
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
                 return
             }
         }
-
-        showError()
+        
+        performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
     }
-
+    
     func parse(json: Data) {
         let decoder = JSONDecoder()
-
+        
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
             filteredPetitions = jsonPetitions.results
-            tableView.reloadData()
+            performSelector(onMainThread: #selector(reloadTableView), with: nil, waitUntilDone: false)
+        } else {
+            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
         }
     }
-
+    
+    @objc func reloadTableView() {
+        tableView.reloadData()
+    }
+    
+    @objc func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    //    override func viewDidLoad() {
+    //        super.viewDidLoad()
+    //        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(creditsTapped))
+    //
+    //        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterTapped))
+    //
+    //        let urlString: String
+    //
+    //        if navigationController?.tabBarItem.tag == 0 {
+    //            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+    //        } else {
+    //            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+    //        }
+    //
+    //        DispatchQueue.global(qos: .userInitiated).async {
+    //            if let url = URL(string: urlString) {
+    //                if let data = try? Data(contentsOf: url) {
+    //                    self.parse(json: data)
+    //                    return
+    //                }
+    //            }
+    //
+    //            self.showError()
+    //        }
+    //    }
+    
+    //    func parse(json: Data) {
+    //        let decoder = JSONDecoder()
+    //
+    //        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+    //            petitions = jsonPetitions.results
+    //            filteredPetitions = jsonPetitions.results
+    //            DispatchQueue.main.async {
+    //                self.tableView.reloadData()
+    //            }
+    //        }
+    //    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return filteredPetitions.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
+        
         let petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
-
+        
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
         vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
-
-    func showError() {
-        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
-    }
+    
+    //    func showError() {
+    //        DispatchQueue.main.async {
+    //             let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+    //             ac.addAction(UIAlertAction(title: "OK", style: .default))
+    //            self.present(ac, animated: true)
+    //        }
+    //    }
     
     func search(for text: String) {
         print(text)
